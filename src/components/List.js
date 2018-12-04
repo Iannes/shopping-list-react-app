a1c6550368aa6200dfe37aaf0146f246729fc367
Yannis Spyrou <spyroyw6@mc-531114.immediate.media>
2018-12-04 16:36:51 +0000

    feat: Enable SW

--- src/components/List.js
+++ src/components/List.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react'
 
 export default class List extends Component {
   render(props) {
+
+    const { list } = this.props
     return (
       <section>
           <ul className="shopping-list">
-                {this.props.list.map(item => {
+                {list && list.map(item => {
                    return <li key={item}>{item}</li>
                 })}
           </ul>
